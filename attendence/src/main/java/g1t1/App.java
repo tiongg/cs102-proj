acd82f0b8ce4438823d15a86363d1527c2837259
Tan Tiong Guan <tantiongguan123@gmail.com>
2025-09-26 23:27:24 +0800

    chore: set default page to past records for now

--- attendence/src/main/java/g1t1/App.java
+++ attendence/src/main/java/g1t1/App.java
@@ -12,7 +12,7 @@ import nu.pattern.OpenCV;
 public class App extends Application {
     public static final int WIDTH = 1440;
     public static final int HEIGHT = 750;
-    private static final PageName initialPage = PageName.Login;
+    private static final PageName initialPage = PageName.PastRecords;
 
     private static App instance;
     private Scene currentScene;
@@ -34,7 +34,7 @@ public class App extends Application {
         Page basePage = Router.scenes.get(initialPage);
         Scene scene = new Scene(basePage.getRoot(), WIDTH, HEIGHT);
         instance.currentScene = scene;
-        
+
         scene.getStylesheets().add(getClass().getResource("css/app.css").toExternalForm());
         scene.getStylesheets().add(getClass().getResource("css/button.css").toExternalForm());
         scene.getStylesheets().add(getClass().getResource("css/stepper.css").toExternalForm());
