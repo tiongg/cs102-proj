a22da35083e7002ade1bd4f72d2314fbe9cf7275
Tan Tiong Guan <tantiongguan123@gmail.com>
2025-09-26 19:10:24 +0800

    fix: override equals method for id classes

--- attendence/src/main/java/g1t1/models/ids/StudentID.java
+++ attendence/src/main/java/g1t1/models/ids/StudentID.java
@@ -4,4 +4,12 @@ public class StudentID extends BaseID {
     public StudentID(String id) {
         super(id);
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof StudentID) {
+            return other.toString().equals(this.toString());
+        }
+        return false;
+    }
 }
