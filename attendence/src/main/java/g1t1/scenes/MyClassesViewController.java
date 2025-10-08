5bb2d8602fd6835fa30f013036642916fc20b37a
Shutowith3e <joey.chik05@gmail.com>
2025-10-08 18:03:14 +0800

    feat: table component for myclasses

--- attendence/src/main/java/g1t1/scenes/MyClassesViewController.java
+++ attendence/src/main/java/g1t1/scenes/MyClassesViewController.java
@@ -1,6 +1,18 @@
 package g1t1.scenes;
 
+// for table 
+import java.util.List;
+import javafx.fxml.FXML;
+import g1t1.components.table.ClassesTable;
+
 import g1t1.models.scenes.PageController;
 
 public class MyClassesViewController extends PageController {
+    @FXML
+    private ClassesTable classesHeader;
+
+    @FXML
+    private void initialize() {
+        classesHeader.setClassesTable(List.of("Module", "Section", "Day", "Time", "Enrolled"));
+    }
 }
